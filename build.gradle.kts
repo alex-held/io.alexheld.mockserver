dc5ad1bfa2265a576a1d9d372333d2940fdebd26
alex held <git@alexanderheld.net>
2020-04-19 16:41:33 +0200

    fixedd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 extra["arrow_version"] = "0.10.4"
 extra["mongodb_version"] = "4.0.2"
@@ -21,40 +21,39 @@ buildscript {
     }
 
     dependencies {
-     //   classpath(kotlin("gradle-plugin", "1.3.72"))
-      //  classpath("org.jetbrains.kotlin:kotlin-allopen:1.3.72")
+        /*classpath(kotlin("gradle-plugin", "1.3.72"))
+        classpath("org.jetbrains.kotlin:kotlin-allopen:1.3.72")*/
     }
 }
 
 
+
 plugins {
+
+    jacoco
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id("org.gradle.kotlin.kotlin-dsl") version "1.3.6"
-
 
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
     kotlin("kapt") version "1.3.72"
 
-  //  id("com.github.ben-manes.versions") version "0.26.0"
-  //  id("com.adarshr.test-logger") version "2.0.0"
+    //  id("com.github.ben-manes.versions") version "0.26.0"
+    //  id("com.adarshr.test-logger") version "2.0.0"
     application
     java
 }
 
-
-
 java {
-    sourceCompatibility = javaversio //JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-application {
-    mainClassName = "io.alexheld.mockserver.MainKt"
-    group = "io.alexheld.mockserver"
-}
 
+application {
+    group = "io.alexheld.mockserver"
+    mainClassName = "io.alexheld.mockserver.MainKt"
+}
 
 val developmentOnly by configurations.creating
 configurations {
@@ -76,40 +75,40 @@ repositories {
 
 dependencies {
 
-    // koin
+// koin
     implementation("org.koin:koin-core:${property("koin_version")}")
     implementation("org.koin:koin-core-ext:${property("koin_version")}")
 
-    // kotlin
+// kotlin
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-   // implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+// implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // jwt
+// jwt
     implementation("io.jsonwebtoken:jjwt:0.9.1")
 
-    // xml
+// xml
     implementation("javax.xml.bind:jaxb-api:${property("jaxb_version")}")
     implementation("com.sun.xml.bind:jaxb-core:${property("jaxb_version")}")
     implementation("com.sun.xml.bind:jaxb-impl:${property("jaxb_version")}")
 
-    // http4k
+// http4k
     implementation("org.http4k:http4k-core:${property("http4k_version")}")
     implementation("org.http4k:http4k-server-jetty:${property("http4k_version")}")
     implementation("org.http4k:http4k-format-jackson:${property("http4k_version")}")
     implementation("org.http4k:http4k-client-apache:${property("http4k_version")}")
 
-    // log4j
+// log4j
     implementation("org.apache.logging.log4j:log4j-core:${property("log4j_version")}")
     implementation("org.apache.logging.log4j:log4j-api:${property("log4j_version")}")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:${property("log4j_version")}")
-    // infrastructure
 
-    // arrow
+
+// arrow
     implementation("io.arrow-kt:arrow-core:${property("arrow_version")}")
-    implementation ("io.arrow-kt:arrow-syntax:${property("arrow_version")}")
+    implementation("io.arrow-kt:arrow-syntax:${property("arrow_version")}")
 
-    // jackson
+// jackson
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${property("jackson_version")}")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-joda:${property("jackson_version")}")
@@ -120,30 +119,30 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${property("jackson_version")}")
 
 
-    // == == == ==
-    // T E S T
-    // == == == ==
+// == == == ==
+// T E S T
+// == == == ==
 
     testImplementation("org.amshove.kluent:kluent:${property("kluent_version")}")
     testImplementation("io.mockk:mockk:1.9.3")
 
 
-    // Test container + Junit5
+// Test container + Junit5
 
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
-    testImplementation( "org.junit.jupiter:junit-jupiter-params:${property("junit_version")}")
-    testRuntimeOnly( "org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
-    //testImplementation("org.junit.jupiter:junit-jupiter:${property("junit_version")}")
-   // testRuntimeOnly( "org.junit.vintage:junit-vintage-engine:${property("junit_version")}")
-    // testImplementation("org.jetbrains.kotlin:kotlin-test-junit:${property("kotlin_version")}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${property("junit_version")}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
+//testImplementation("org.junit.jupiter:junit-jupiter:${property("junit_version")}")
+// testRuntimeOnly( "org.junit.vintage:junit-vintage-engine:${property("junit_version")}")
+// testImplementation("org.jetbrains.kotlin:kotlin-test-junit:${property("kotlin_version")}")
 
 
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+    //testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
     testImplementation("org.testcontainers:testcontainers:1.14.0")
     testImplementation("org.testcontainers:junit-jupiter:1.14.0")
 
 
-    // Infra
+// Infra
     implementation("org.litote.kmongo:kmongo:4.0.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
@@ -152,25 +151,34 @@ dependencies {
 
 
     // Annotation
-    //kapt("io.arrow-kt:arrow-meta:${property("arrow_version")}")
-    /*compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")*/
+    kapt("io.arrow-kt:arrow-meta:${property("arrow_version")}")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+
 }
 
+
 /*
+
 tasks.jacocoTestReport {
     reports {
         xml.isEnabled = true
     }
 }*/
 
+
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget= "1.8"
-    kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
+tasks.withType<KotlinCompile>().configureEach {
+    println("Configuring $name in project ${project.name}...")
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        suppressWarnings = true
+    }
 }
 
